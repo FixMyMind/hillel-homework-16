75d69638455c635f00dd10bacfe5f8aa34c5c2ec
Oleh Lukianov <oleg.lukianov@gmail.com>
2024-03-17 13:49:13 +0200

    Homework solution

---
+++ src/DecryptApp.java
@@ -0,0 +1,6 @@
+public class DecryptApp {
+    public static void main(String[] args) {
+        Decryptor decryptedMessage = new Decryptor();
+        decryptedMessage.decryptMessage("-$''$<k<#\"?.k9*))\"?", "%&");
+    }
+}
